f7693bfa573746571a5ac197075b7bd9b6a03e13
shimizu.kenji <shimizu.kenji@noncore.net>
2018-09-24 23:31:52 +0900

    プロジェクト作成

---
+++ src/main/java/net/noncore/fdx/FdxApplication.java
@@ -0,0 +1,12 @@
+package net.noncore.fdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdxApplication.class, args);
+	}
+}
